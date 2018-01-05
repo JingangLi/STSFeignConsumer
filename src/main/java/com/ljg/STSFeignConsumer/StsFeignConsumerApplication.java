0bfa35794d8b2651c52ac2ddf25c8a06d03e6409
JingangLi <ljg_emails@sina.com>
2018-01-05 07:43:09 +0100

    Create Feign project

---
+++ src/main/java/com/ljg/STSFeignConsumer/StsFeignConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.ljg.STSFeignConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class StsFeignConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsFeignConsumerApplication.class, args);
+	}
+}
