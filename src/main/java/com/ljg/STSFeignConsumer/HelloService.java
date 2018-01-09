9968c76954d65717f4c6ddd35df540b6293bd776
JingangLi <ljg_emails@sina.com>
2018-01-09 08:02:09 +0100

    Support Http request param and header param

--- src/main/java/com/ljg/STSFeignConsumer/HelloService.java
+++ src/main/java/com/ljg/STSFeignConsumer/HelloService.java
@@ -1,12 +1,25 @@
 package com.ljg.STSFeignConsumer;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("hello-service")
 public interface HelloService {
 	
-	@RequestMapping("/hello")
+	@RequestMapping( "/hello")
 	String hello();
+	
+	@RequestMapping( value = "/hello1", method = RequestMethod.GET )
+	String hello( @RequestParam("name") String name );
+	
+	@RequestMapping( value = "/hello2", method = RequestMethod.GET )
+	String hello( @RequestHeader("name") String name, @RequestHeader("age") Integer age );
+	
+	@RequestMapping( value = "/hello3", method = RequestMethod.GET )
+	String hello( @RequestBody User user);
 
 }
